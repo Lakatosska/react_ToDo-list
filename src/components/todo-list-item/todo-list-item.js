00afdee0d5933ac7d2bc5fa1af0d7519cc9feb6b
Lakatosska <vizualnoe@gmail.com>
2022-04-11 01:26:41 +0300

    mark important by clicking

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -19,6 +19,7 @@ export default class TodoListItem extends React.Component {
 
   state = {
     done: false,
+    important: false
   };
 
   // передаем в функцию setState объект с изменениями, которые хотим внести в state
@@ -29,35 +30,40 @@ export default class TodoListItem extends React.Component {
     })
   };
 
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    });
+  };
+
 
   render() {
 
-    const { label, important = false } = this.props; 
-    const { done } = this.state;
+    const { label } = this.props; 
+    const { done, important } = this.state;
 
     let classNames = "todo-list-item";
     if (done) {
       classNames += " done";
     }
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    if (important) {
+      classNames += " important";
+    }
 
     return (
       // если добавить done, то будет зачеркнуто
       <span className={classNames}> 
         <span
           className="todo-list-item-label"
-          style={style}
           onClick={ this.onLabelClick }
         >
           {label}
         </span>
   
         <button type="button"
-            className="btn btn-outline-success btn-sm float-right">
+            className="btn btn-outline-success btn-sm float-right"
+            onClick={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
   
